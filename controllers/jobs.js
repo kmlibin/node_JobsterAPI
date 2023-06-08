09c8062d6627d3027d06b660964ebf3b56c70370
Kelli Libin <km.putnam2@gmail.com>
2023-06-08 11:02:14 -0600

    set up pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -22,6 +22,7 @@ const getAllJobs = async (req, res) => {
   }
   //we are chaining some things after we perform the query, so no await here. find results w/ q object, can't chain after await
   let result = Job.find(queryObject);
+
   //we have the results, so now we sort the object
   if (sort === "latest") {
     result = result.sort("-createdAt");
@@ -36,8 +37,20 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position");
   }
 
+  //pagination
+  const page = Number(req.query.page) || 1; //have q param set up for page. don't have one set up for limit
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  //send back response
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 const getJob = async (req, res) => {
   const {
